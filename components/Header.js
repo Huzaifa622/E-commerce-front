c2033afb93c93f3569117fa33721d1628180860d
unknown <thuzaifa07@gmail.com>
2023-10-23 20:54:34 +0500

    ecommerce

---
+++ components/Header.js
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import styled from "styled-components";
+import Center from "./Center";
+const StyledHeader = styled.header`
+  background-color: #191919;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled(Link)`
+  color: #f8f8f8;
+  text-decoration: none;
+  padding: 20px 0;
+  font-weight: bolder;
+  font-size: larger;
+`;
+const NavLink= styled(Link)`
+color: #686461;
+text-decoration: none;
+padding: 10px 0;
+`;
+
+const StyledNav = styled.nav`
+display: flex;
+gap: 15px;
+`
+export default function Header() {
+  return (
+    <>
+      <StyledHeader>
+        <Center>
+          <Wrapper>
+            <Logo href={"/"}>E-Commerce</Logo>
+            <StyledNav>
+              <NavLink href={""}>Home</NavLink>
+              <NavLink href={""}>All Products</NavLink>
+              <NavLink href={""}>Categories</NavLink>
+              <NavLink href={""}>Account</NavLink>
+              <NavLink href={""}>Cart (0)</NavLink>
+            </StyledNav>
+          </Wrapper>
+        </Center>
+      </StyledHeader>
+    </>
+  );
+}
