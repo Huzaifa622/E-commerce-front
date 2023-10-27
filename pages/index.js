34ef0d9daa5137bbb7354123258b323052c0ca90
unknown <thuzaifa07@gmail.com>
2023-10-27 21:10:54 +0500

    ecommerce

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
 import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import { Product } from "@/components/models/Product";
+import { mongooseConnect } from "@/lib/mongoose";
 
-export default function HomePage(){
+export default function HomePage({ product }) {
+  console.log(product)
   return (
     <>
-    <Header/>
-    <Featured/>
+      <Header />
+      <Featured />
     </>
-  )
+  );
 }
 
-export function getServerSideProps(){
-
-  
+export async function getServerSideProps() {
+  const featuredProductId = "65339e3765f2f555b4f337e2";
+  await mongooseConnect();
+  const product = await Product.findById(featuredProductId);
+  return {
+    props: {
+      product: JSON.parse(JSON.stringify(product)),
+    },
+  };
 }
